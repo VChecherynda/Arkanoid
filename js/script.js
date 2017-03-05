3cb5c3ee94e159fcad1475c890051b4c9034f1d0
Vadym <vadosevich@gmail.com>
2017-03-05 15:41:13 +0200

    Bounce from the edges

--- js/script.js
+++ js/script.js
@@ -1,8 +1,33 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
+var x = canvas.width/2;
+var y = canvas.height-40;
+var dx = 2;
+var dy = -2;
+var ballRadius = 10;
 
-ctx.beginPath();
-ctx.rect(20,40,50,50);
-ctx.fillStyle = "#ff0000";
-ctx.fill();
-ctx.closePath();
+function drawBall() {
+  ctx.beginPath();
+  ctx.arc(x, y, ballRadius , 0, Math.PI*2);
+  ctx.fillStyle = "#218CD4";
+  ctx.fill();
+  ctx.closePath();
+
+  if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+    dx = - dx;
+  }
+
+  if(y + dy > canvas.width-ballRadius || y + dy < ballRadius) {
+    dy = - dy;
+  }
+
+}
+
+function draw() {
+  ctx.clearRect(0,0, canvas.width, canvas.height);
+  drawBall();
+  x += dx;
+  y += dy;
+}
+
+setInterval(draw, 10);
